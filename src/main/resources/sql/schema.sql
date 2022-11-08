6eafb8cc3a18fdd7f6b6324db16651541bab41f6
Iseason2000 <Iseason2000@qq.com>
2022-11-08 22:49:03 +0800

    update

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -1,14 +1,17 @@
 CREATE SCHEMA IF NOT EXISTS `CM_System`;
+
 use `CM_System`;
+
 CREATE TABLE IF NOT EXISTS `CM_System`.`base_user`
 (
-    `id`       int primary key not null auto_increment,
-    `mail`     varchar(255)    not null,
-    `password` varchar(255)    not null,
-    `role`     int             not null,
+    `id`       int primary key not null auto_increment comment '用户id',
+    `mail`     varchar(255)    not null comment '用户邮箱',
+    `password` varchar(255)    not null comment '用户密码',
+    `role`     int             not null comment '用户权限',
     unique index base_user_mail (`mail`),
     unique index base_user_mail_password (`mail`, `password`)
 );
+
 -- 默认账号 admin@mail.com admin123654
 insert ignore into `CM_System`.`base_user` (id, mail, password, role)
 values (1, 'admin@mail.com', '$2a$10$DiPF2HTX9Uzvuy83CRh4jOWzm1ZwC7CH4A6o7dXBTN9h7HiDZBv7G', 0);
